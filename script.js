84b1ad224b828eeb71c7da5d001b5ea65ea313e9
CulturalHeritageOfLahore <alphaq990@gmail.com>
2022-11-12 20:20:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,346 @@
+function showDetails1(){
+    document.getElementById("artifact-detail-1").style.visibility = "visible";
+}
+
+function hideDetails1(){
+    document.getElementById("artifact-detail-1").style.visibility = "hidden";
+}
+
+function showDetails2(){
+    document.getElementById("artifact-detail-2").style.visibility = "visible";
+}
+
+function hideDetails2(){
+    document.getElementById("artifact-detail-2").style.visibility = "hidden";
+}
+
+function showDetails3(){
+    document.getElementById("artifact-detail-3").style.visibility = "visible";
+}
+
+function hideDetails3(){
+    document.getElementById("artifact-detail-3").style.visibility = "hidden";
+}
+
+function showDetails4(){
+    document.getElementById("artifact-detail-4").style.visibility = "visible";
+}
+
+function hideDetails4(){
+    document.getElementById("artifact-detail-4").style.visibility = "hidden";
+}
+
+function showDetails5(){
+    document.getElementById("artifact-detail-5").style.visibility = "visible";
+  
+}
+
+function hideDetails5(){
+    document.getElementById("artifact-detail-5").style.visibility = "hidden";
+    }
+
+function showDetails6(){
+    document.getElementById("artifact-detail-6").style.visibility = "visible";
+    
+}
+
+function hideDetails6(){
+    document.getElementById("artifact-detail-6").style.visibility = "hidden";
+}
+
+function showDetails8(){
+    document.getElementById("artifact-detail-8").style.visibility = "visible";
+}
+
+function hideDetails8(){
+    document.getElementById("artifact-detail-8").style.visibility = "hidden";
+}
+
+function showDetails9(){
+    document.getElementById("artifact-detail-9").style.visibility = "visible";
+}
+
+function hideDetails9(){
+    document.getElementById("artifact-detail-9").style.visibility = "hidden";
+}
+
+function showDetails10(){
+    document.getElementById("artifact-detail-10").style.visibility = "visible";
+}
+
+function hideDetails10(){
+    document.getElementById("artifact-detail-10").style.visibility = "hidden";
+}
+
+function showDetails11(){
+    document.getElementById("artifact-detail-11").style.visibility = "visible";
+}
+
+function hideDetails11(){
+    document.getElementById("artifact-detail-11").style.visibility = "hidden";
+}
+
+function showDetails12(){
+    document.getElementById("artifact-detail-12").style.visibility = "visible";
+}
+
+function hideDetails12(){
+    document.getElementById("artifact-detail-12").style.visibility = "hidden";
+}
+
+function showDetails13(){
+    document.getElementById("artifact-detail-13").style.visibility = "visible";
+}
+
+function hideDetails13(){
+    document.getElementById("artifact-detail-13").style.visibility = "hidden";
+}
+
+function showDetails14(){
+    document.getElementById("artifact-detail-14").style.visibility = "visible";
+}
+
+function hideDetails14(){
+    document.getElementById("artifact-detail-14").style.visibility = "hidden";
+}
+
+function showDetails15(){
+    document.getElementById("artifact-detail-15").style.visibility = "visible";
+}
+
+function hideDetails15(){
+    document.getElementById("artifact-detail-15").style.visibility = "hidden";
+}
+
+function showDetails16(){
+    document.getElementById("artifact-detail-16").style.visibility = "visible";
+}
+
+function hideDetails16(){
+    document.getElementById("artifact-detail-16").style.visibility = "hidden";
+}
+
+function showDetails18(){
+    document.getElementById("artifact-detail-18").style.visibility = "visible";
+}
+
+function hideDetails18(){
+    document.getElementById("artifact-detail-18").style.visibility = "hidden";
+}
+
+function showDetails19(){
+    document.getElementById("artifact-detail-19").style.visibility = "visible";
+}
+
+function hideDetails19(){
+    document.getElementById("artifact-detail-19").style.visibility = "hidden";
+}
+
+function showDetails20(){
+    document.getElementById("artifact-detail-20").style.visibility = "visible";
+}
+
+function hideDetails20(){
+    document.getElementById("artifact-detail-20").style.visibility = "hidden";
+}
+
+function showDetails21(){
+    document.getElementById("artifact-detail-21").style.visibility = "visible";
+}
+
+function hideDetails21(){
+    document.getElementById("artifact-detail-21").style.visibility = "hidden";
+}
+
+function showDetails22(){
+    document.getElementById("artifact-detail-22").style.visibility = "visible";
+}
+
+function hideDetails22(){
+    document.getElementById("artifact-detail-22").style.visibility = "hidden";
+}
+
+function showDetails23(){
+    document.getElementById("artifact-detail-23").style.visibility = "visible";
+}
+
+function hideDetails23(){
+    document.getElementById("artifact-detail-23").style.visibility = "hidden";
+}
+
+function showDetails24(){
+    document.getElementById("artifact-detail-24").style.visibility = "visible";
+}
+
+function hideDetails24(){
+    document.getElementById("artifact-detail-24").style.visibility = "hidden";
+}
+
+function showDetails25(){
+    document.getElementById("artifact-detail-25").style.visibility = "visible";
+}
+
+function hideDetails25(){
+    document.getElementById("artifact-detail-25").style.visibility = "hidden";
+}
+
+function showDetails26(){
+    document.getElementById("artifact-detail-26").style.visibility = "visible";
+}
+
+function hideDetails26(){
+    document.getElementById("artifact-detail-26").style.visibility = "hidden";
+}
+
+function showDetails27(){
+    document.getElementById("artifact-detail-27").style.visibility = "visible";
+}
+
+function hideDetails27(){
+    document.getElementById("artifact-detail-27").style.visibility = "hidden";
+}
+
+
+function showDetails28(){
+    document.getElementById("artifact-detail-28").style.visibility = "visible";
+}
+
+function hideDetails28(){
+    document.getElementById("artifact-detail-28").style.visibility = "hidden";
+}
+
+function showDetails29(){
+    document.getElementById("artifact-detail-29").style.visibility = "visible";
+}
+
+function hideDetails29(){
+    document.getElementById("artifact-detail-29").style.visibility = "hidden";
+}
+
+function showDetails30(){
+    document.getElementById("artifact-detail-30").style.visibility = "visible";
+}
+
+function hideDetails30(){
+    document.getElementById("artifact-detail-30").style.visibility = "hidden";
+}
+
+function showDetails31(){
+    document.getElementById("artifact-detail-31").style.visibility = "visible";
+}
+
+function hideDetails31(){
+    document.getElementById("artifact-detail-31").style.visibility = "hidden";
+}
+
+function showDetails32(){
+    document.getElementById("artifact-detail-32").style.visibility = "visible";
+}
+
+function hideDetails32(){
+    document.getElementById("artifact-detail-32").style.visibility = "hidden";
+}
+
+function showDetails33(){
+    document.getElementById("artifact-detail-33").style.visibility = "visible";
+}
+
+function hideDetails33(){
+    document.getElementById("artifact-detail-33").style.visibility = "hidden";
+}
+
+function showDetails34(){
+    document.getElementById("artifact-detail-34").style.visibility = "visible";
+}
+
+function hideDetails34(){
+    document.getElementById("artifact-detail-34").style.visibility = "hidden";
+}
+
+function showDetails35(){
+    document.getElementById("artifact-detail-35").style.visibility = "visible";
+}
+
+function hideDetails35(){
+    document.getElementById("artifact-detail-35").style.visibility = "hidden";
+}
+
+function showDetails36(){
+    document.getElementById("artifact-detail-36").style.visibility = "visible";
+}
+
+function hideDetails36(){
+    document.getElementById("artifact-detail-36").style.visibility = "hidden";
+}
+
+function showDetails37(){
+    document.getElementById("artifact-detail-37").style.visibility = "visible";
+}
+
+function hideDetails37(){
+    document.getElementById("artifact-detail-37").style.visibility = "hidden";
+}
+
+function showDetails38(){
+    document.getElementById("artifact-detail-38").style.visibility = "visible";
+}
+
+function hideDetails38(){
+    document.getElementById("artifact-detail-38").style.visibility = "hidden";
+}
+
+function showDetails39(){
+    document.getElementById("artifact-detail-39").style.visibility = "visible";
+}
+
+function hideDetails39(){
+    document.getElementById("artifact-detail-39").style.visibility = "hidden";
+}
+
+function showDetails40(){
+    document.getElementById("artifact-detail-40").style.visibility = "visible";
+}
+
+function hideDetails40(){
+    document.getElementById("artifact-detail-40").style.visibility = "hidden";
+}
+
+function showDetails41(){
+    document.getElementById("artifact-detail-41").style.visibility = "visible";
+}
+
+function hideDetails41(){
+    document.getElementById("artifact-detail-41").style.visibility = "hidden";
+}
+
+function showDetails42(){
+    document.getElementById("artifact-detail-42").style.visibility = "visible";
+}
+
+function hideDetails42(){
+    document.getElementById("artifact-detail-42").style.visibility = "hidden";
+}
+
+function showDetails43(){
+    document.getElementById("artifact-detail-43").style.visibility = "visible";
+}
+
+function hideDetails43(){
+    document.getElementById("artifact-detail-43").style.visibility = "hidden";
+}
+
+function showDetails44(){
+    document.getElementById("artifact-detail-44").style.visibility = "visible";
+}
+
+function hideDetails44(){
+    document.getElementById("artifact-detail-44").style.visibility = "hidden";
+}
+
+function showDetails45(){
+    document.getElementById("artifact-detail-45").style.visibility = "visible";
+}
+
+function hideDetails45(){
+    document.getElementById("artifact-detail-45").style.visibility = "hidden";
+}
